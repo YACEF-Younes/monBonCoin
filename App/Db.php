76f9bd2bd3389e38b5cdc40c4a4f1b1e21f04c49
YACEF-Younes <138567041+YACEF-Younes@users.noreply.github.com>
2023-07-24 13:54:49 +0200

    connect BDD au php

---
+++ App/Db.php
@@ -0,0 +1,35 @@
+<?php
+namespace App;
+
+use PDO;
+
+use PDOException;
+
+// Cette classe permet de se connecter à la BDD en utilisant le pattern Singleton
+class Db{
+    private static $db; // pour stocker mon objet PDO
+
+    // Singleton
+    static function getDb(){
+        if(!self::$db){
+            try {
+                $config = file_get_contents('../App/config.json');
+                // var_dump($config);
+                // Pour pouvoir utiliser un fichier json il faut le decoder
+                $config = json_decode($config);
+                // var_dump($config);
+                // echo "test";
+                // On crée l'objet PDO
+                self::$db = new PDO("mysql:host=" . $config->host . ";dbname=" .$config->dbName, $config->user,$config->password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            } catch (PDOException $err) {
+                echo "problème de connexion à la base de donnée : " . $err->getMessage();
+            }
+        }
+        return self::$db;
+    }
+}
+
+$objetdB = new Db;
+
+$objetdB::getDb();
+
