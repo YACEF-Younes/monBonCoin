a49a354e81541aea3aea56af2ad8b54073d69f0f
YACEF-Younes <138567041+YACEF-Younes@users.noreply.github.com>
2023-07-24 17:01:01 +0200

    crud USER

--- Models/Users.php
+++ Models/Users.php
@@ -21,8 +21,50 @@ class Users extends Db{
     
 
     //2/ Méthode pour trouver un user par son id
+    public static function findById($id){
+        $request = 'SELECT * FROM users WHERE idUser = :id';
+        $response = self::getDb()->prepare($request);
+        $response->bindValue(':id', $id, PDO::PARAM_INT);
+        $response->execute();
 
-    //3/ Méthode pour trouver un user par son login
+        return $response->fetch(PDO::FETCH_ASSOC);
+    }
+    //2/ Méthode pour touver un user par son login
+    public static function findByLogin($login){
+        $request = 'SELECT * FROM users WHERE login = :login';
+        $response = self::getDb()->prepare($request);
+        $response->bindValue(':login', $login, PDO::PARAM_STR);
+        $response->execute();
 
+        return $response->fetch(PDO::FETCH_ASSOC);
+    }
 
+    ///////////////// Méthode d'écritue
+    // 1/ Méthode pour créer un user
+    public static function create(array $data){
+        // Syntaxe sans les bindValue utilisation di "?"
+        // Avec cette $data doit être un tableau qui contient toutes les valeurs à enregistrer en BDD
+        $request = "INSERT INTO users (login, password, firstName, lastName, adress, cp, city) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        $response = self::getDb()->prepare($request);
+        return $response->execute($data);
+    }
+
+    // 2/ Méthode pour modifier un user
+    public static function update(array $data){
+        $request = "UPDATE users SET login = ?, password = ?, firstName = ?, lastName= ?, adress = ?, cp = ?, city = ? WHERE idUser = ?";
+        $response = self::getDb()->prepare($request);
+        return $response->execute($data);
+    }
+
+    // 3/ Méthode de suppression
+    public static function delete($id){
+        $request = "DELETE FROM users WHERE idUser = :id";
+        $response = self::getDb()->prepare($request);
+        $response->bindValue(':id', $id, PDO::PARAM_INT);
+        $response->execute();
+
+        return $response->fetch(PDO::FETCH_ASSOC);
+    }
+
+    
 }
--- index.php
+++ index.php
@@ -25,3 +25,42 @@ require_once('autoloader.php');
 $objetDb = new Db;
 $users = Users::findAll();
 var_dump($users);
+?>
+
+<h2>Utilisation de la méthode findById sur users</h2>
+
+<?php
+$idUsers = new Users;
+var_dump($idUsers->findById(1));
+?>
+
+<h2>Utilisation de la méthode findByLogin sur users</h2>
+
+<?php
+$loginUsers = new Users;
+var_dump($loginUsers->findByLogin('user1@gmail.com'));
+?>
+
+<h2>Utilisation de la methode create() sur users</h2>
+<?php
+    $pass = password_hash('12345678', PASSWORD_DEFAULT);
+    $data = ['jarjar@gmail.com', $pass, 'jarjar', 'Binks', '21 rue de Sheev', '93333', 'Naboo', 3];
+    // A chaque fois qu'on relance ca creer un nouveau user
+    // Users::create($data); 
+
+   
+?>
+
+<h2>Utilisation de la methode create() sur users</h2>
+<?php
+Users::update($data);
+
+?>
+
+
+<h2>Utilisation de la methode create() sur users</h2>
+
+<?php
+Users::delete(3);
+
+?>
